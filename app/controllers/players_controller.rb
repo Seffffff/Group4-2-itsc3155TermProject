e96d77ca1fb9126c20915fdca6b9b304db722531
Martin Katz <mkatz3@uncc.edu>
2018-04-02 17:44:45 +0000

    complete create and show

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,20 @@
 class PlayersController < ApplicationController
+    def show
+        @player = Player.find(params[:id])
+    end
     def new
     end
     
     def create
-        render plain: params[:player].inspect
+        @player = Player.new(player_params)
+        
+        @player.save
+        redirect_to @player
     end
 end
+
+private
+    def player_params
+        params.require(:player).permit(:name, :teacher, :grade)
+    end
+    
